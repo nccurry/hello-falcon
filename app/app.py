e0971db6c5154f5e34ee23b87071da0ec7150ae8
Nick Curry <ncc@nixtopt460s.attlocal.net>
2017-09-28 13:29:42 -0500

    Modified app and template

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import falcon
-
+import json
 
 class Index(object):
 
@@ -7,15 +7,24 @@ class Index(object):
         resp.status = falcon.HTTP_200
         resp.body = '<html>Hello world</html>'
 
+        print("Received GET")
+
+    def on_post(self, req, resp):
+        resp.status = falcon.HTTP_201
+        resp.body = req.stream.read()
+        
+        print("Received POST")
+
 
 class HealthCheck(object):
 
     def on_get(self, req, resp):
         resp.status = falcon.HTTP_200
-        resp.body = 'ok'
+        resp.body = json.dumps({'status':'ok'})
 
+        print("Recieved Health Check")
 
-app = falcon.API(media_type="text/html")
+app = falcon.API()
 
 app.add_route('/', Index())
 app.add_route('/healthz', HealthCheck())
